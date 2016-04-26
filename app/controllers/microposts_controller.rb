08cb05d0eb19cd4a3a9937c584aba86c0c5147bb
akira_tameoka <tameoka.akira.kakaku@gmail.com>
2016-04-26 01:18:17 +0000

    feed created.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -13,11 +13,14 @@ class MicropostsController < ApplicationController
   
   def destroy
     @micropost = current_user.microposts.find_by(id: params[:id])
-    return redirect_to root_url if @micropost.nil?
-    @micropost.destroy
-    flash[:success] = "Micropost deleted"
-    redirect_to request.referrer || root_url
-  end  
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc) # この行を追加
+      render 'static_pages/home'
+    end
+  end
   
   private
   def micropost_params
